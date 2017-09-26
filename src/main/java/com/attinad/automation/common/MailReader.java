ccf5c36a56f28e634b32eb1b202d93e578fc59c9
josemaria <josemaria.thomas@attinadsoftware.com>
2017-09-26 15:05:01 +0530

    SonarQube bug fixes

--- src/main/java/com/attinad/automation/common/MailReader.java
+++ src/main/java/com/attinad/automation/common/MailReader.java
@@ -16,41 +16,51 @@ import com.attinad.automation.utils.CantizAutomationCoreException;
 import org.jsoup.Jsoup;
 
 /**
- * Created by anandxp (anand.xavier@attinadsoftware.com) on 17/01/17.
- * This utility class can be used to read mails from various mail servers. The class
- * supports imap, pop3 and smtp mail servers. Configuration for the mail servers can be provided
- * to this class via method signatures or using property files.
+ * Created by anandxp (anand.xavier@attinadsoftware.com) on 17/01/17. This
+ * utility class can be used to read mails from various mail servers. The class
+ * supports imap, pop3 and smtp mail servers. Configuration for the mail servers
+ * can be provided to this class via method signatures or using property files.
  */
 public class MailReader {
 
 	public static String getMailContent() throws CantizAutomationCoreException {
 		PropertyReader propertyReader = PropertyReader.getInstance();
-		if(null == propertyReader.getMailUserName())
-			throw new CantizAutomationCoreException(Constants.MAIL_USERNAME_PROPERTY+" should be defined in application configuration file");
-		if(null == propertyReader.getMailPassword())
-			throw new CantizAutomationCoreException(Constants.MAIL_PSWD_PROPERTY+" should be defined in application configuration file");
-		if(null == propertyReader.getMailStoreType())
-			throw new CantizAutomationCoreException(Constants.MAIL_STORE_TYPE_PROPERTY+" should be defined in application configuration file");
-		if(null == propertyReader.getMailHost())
-			throw new CantizAutomationCoreException(Constants.MAIL_HOST_PROPERTY+" should be defined in application configuration file");
-		if(null == propertyReader.getMailPort())
-			throw new CantizAutomationCoreException(Constants.MAIL_PORT_PROPERTY+" should be defined in application configuration file");
-		return getMailContent(propertyReader.getMailUserName(),propertyReader.getMailPassword());
+		if (null == propertyReader.getMailUserName())
+			throw new CantizAutomationCoreException(
+					Constants.MAIL_USERNAME_PROPERTY + " should be defined in application configuration file");
+		if (null == propertyReader.getMailPassword())
+			throw new CantizAutomationCoreException(
+					Constants.MAIL_PSWD_PROPERTY + " should be defined in application configuration file");
+		if (null == propertyReader.getMailStoreType())
+			throw new CantizAutomationCoreException(
+					Constants.MAIL_STORE_TYPE_PROPERTY + " should be defined in application configuration file");
+		if (null == propertyReader.getMailHost())
+			throw new CantizAutomationCoreException(
+					Constants.MAIL_HOST_PROPERTY + " should be defined in application configuration file");
+		if (null == propertyReader.getMailPort())
+			throw new CantizAutomationCoreException(
+					Constants.MAIL_PORT_PROPERTY + " should be defined in application configuration file");
+		return getMailContent(propertyReader.getMailUserName(), propertyReader.getMailPassword());
 	}
 
-	public static String getMailContent(String user,String password) throws CantizAutomationCoreException {
+	public static String getMailContent(String user, String password) throws CantizAutomationCoreException {
 		PropertyReader propertyReader = PropertyReader.getInstance();
-		if(null == propertyReader.getMailHost())
-			throw new CantizAutomationCoreException(Constants.MAIL_HOST_PROPERTY+" should be defined in application configuration file");
-		if(null == propertyReader.getMailStoreType())
-			throw new CantizAutomationCoreException(Constants.MAIL_STORE_TYPE_PROPERTY+" should be defined in application configuration file");
-		if(null == propertyReader.getMailPort())
-			throw new CantizAutomationCoreException(Constants.MAIL_PORT_PROPERTY+" should be defined in application configuration file");
-		return getMailContent(propertyReader.getMailHost(),propertyReader.getMailPort(),propertyReader.getMailStoreType(),user,password);
+		if (null == propertyReader.getMailHost())
+			throw new CantizAutomationCoreException(
+					Constants.MAIL_HOST_PROPERTY + " should be defined in application configuration file");
+		if (null == propertyReader.getMailStoreType())
+			throw new CantizAutomationCoreException(
+					Constants.MAIL_STORE_TYPE_PROPERTY + " should be defined in application configuration file");
+		if (null == propertyReader.getMailPort())
+			throw new CantizAutomationCoreException(
+					Constants.MAIL_PORT_PROPERTY + " should be defined in application configuration file");
+		return getMailContent(propertyReader.getMailHost(), propertyReader.getMailPort(),
+				propertyReader.getMailStoreType(), user, password);
 	}
 
 	/**
-	 * This method should be called if mail settings needs to be passed directly to automation core.
+	 * This method should be called if mail settings needs to be passed directly to
+	 * automation core.
 	 *
 	 * @param host
 	 * @param port
@@ -60,7 +70,8 @@ public class MailReader {
 	 * @return
 	 * @throws CantizAutomationCoreException
 	 */
-	public static String getMailContent(String host, String port, String storeType, String user, String password) throws CantizAutomationCoreException {
+	public static String getMailContent(String host, String port, String storeType, String user, String password)
+			throws CantizAutomationCoreException {
 		String result = "";
 		Store store = null;
 		Folder emailFolder = null;
@@ -94,41 +105,46 @@ public class MailReader {
 				store = emailSession.getStore("imaps");
 				store.connect(host, user, password);
 			}
- 
-			emailFolder = store.getFolder("INBOX");
+			try {
+				emailFolder = store.getFolder("INBOX");
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+				return result;
+			}
+
 			emailFolder.open(Folder.HOLDS_MESSAGES);
 
 			// retrieve the messages from the folder in an array and print it
 			Message[] messages = emailFolder.getMessages();
 			int msglength = messages.length;
-			if(msglength == 0)
+			if (msglength == 0)
 				return result;
-				// length - 1 gives the latest message
-				Message message = messages[msglength - 1];
+			// length - 1 gives the latest message
+			Message message = messages[msglength - 1];
 
-				Object content = message.getContent();
-				// String result = "";
-				if (content instanceof String) {
-					String body = (String) content;
-					result = body;
-				} else if (content instanceof Multipart) {
+			Object content = message.getContent();
+			// String result = "";
+			if (content instanceof String) {
+				String body = (String) content;
+				result = body;
+			} else if (content instanceof Multipart) {
 
-					MimeMultipart mimeMultipart = (MimeMultipart) content;
-					int count = mimeMultipart.getCount();
+				MimeMultipart mimeMultipart = (MimeMultipart) content;
+				int count = mimeMultipart.getCount();
 
-					for (int i = 0; i < count; i++) {
-						BodyPart bodyPart = mimeMultipart.getBodyPart(i);
-						if (bodyPart.isMimeType("text/plain")) {
-							result = result + "\n" + bodyPart.getContent();
-							break; // without break same text appears twice in
-									// my
-									// tests
-						} else if (bodyPart.isMimeType("text/html")) {
-							String html = (String) bodyPart.getContent();
-							result = result + "\n" + Jsoup.parse(html).text();
-						}
+				for (int i = 0; i < count; i++) {
+					BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+					if (bodyPart.isMimeType("text/plain")) {
+						result = result + "\n" + bodyPart.getContent();
+						break; // without break same text appears twice in
+								// my
+								// tests
+					} else if (bodyPart.isMimeType("text/html")) {
+						String html = (String) bodyPart.getContent();
+						result = result + "\n" + Jsoup.parse(html).text();
 					}
 				}
+			}
 			return result;
 
 		} catch (MessagingException e) {
@@ -136,15 +152,15 @@ public class MailReader {
 			throw new CantizAutomationCoreException(e.getMessage());
 		} catch (IOException e) {
 			throw new CantizAutomationCoreException(e.getMessage());
-		} finally{
-			if(null != emailFolder && emailFolder.isOpen()){
+		} finally {
+			if (null != emailFolder && emailFolder.isOpen()) {
 				try {
 					emailFolder.close(false);
 				} catch (MessagingException e) {
 					e.printStackTrace();
 				}
 			}
-			if(null != store && store.isConnected()){
+			if (null != store && store.isConnected()) {
 				try {
 					store.close();
 				} catch (MessagingException e) {
