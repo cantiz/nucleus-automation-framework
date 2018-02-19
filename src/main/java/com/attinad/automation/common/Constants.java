c45e4b52ee7b1d48ee7a21fbae6ce772fbdd889d
Shariq <shariq.m@attinadsoftware.com>
2018-02-19 15:32:35 +0530

    edited super user token constant

--- src/main/java/com/attinad/automation/common/Constants.java
+++ src/main/java/com/attinad/automation/common/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static final String TARGET_URL_PROPERTY = "cantiz.automation.environment.target-url";
     public static final String TARGET_ELECTRONURL_PROPERTY = "cantiz.automation.environment.target-electronurl";
     public static final String ELECTRON_TOKEN_PROPERTY = "cantiz.automation.environment.token";
-    public static final String SUPERUSER_TOKEN_PROPERTY = "antiz.automation.environment.superUserToken";
+    public static final String SUPERUSER_TOKEN_PROPERTY = "cantiz.automation.environment.superUserToken";
     
 
     public static final String MAIL_HOST_PROPERTY = "cantiz.automation.mailer.host";
