8a1ecd131833b84643b9b8f63d998d2ade6db0ff
josemaria <josemaria.thomas@attinadsoftware.com>
2018-03-14 12:50:22 +0530

    updated core version to 1.0.12

--- src/main/java/com/attinad/automation/testmanager/PractiTestVariables.java
+++ src/main/java/com/attinad/automation/testmanager/PractiTestVariables.java
@@ -1,7 +1,6 @@
 package com.attinad.automation.testmanager;
 
 public class PractiTestVariables {
-	public static String TOKENVALUE = "";
 	public static String TESTID = "";
 	public static String TESTSETID = "";
 	public static String TESTINSTANCEID = "";
